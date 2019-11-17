7be6c342f304bdd2602f17d49338d176364f3d0d
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2019-11-17 22:04:57 +0100

    Removed platform

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ImageDecoder",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_11)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
