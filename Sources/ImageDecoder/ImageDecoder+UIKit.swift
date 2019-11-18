8db72df2c66a5f4ab3bfb446bea511a7275f83da
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2019-11-18 06:04:05 +0100

    Separate static and animated UIImage creation

--- Sources/ImageDecoder/ImageDecoder+UIKit.swift
+++ Sources/ImageDecoder/ImageDecoder+UIKit.swift
@@ -11,32 +11,38 @@ import UIKit
 @available(iOS 9.0, macOS 10.11, *)
 extension ImageDecoder {
 
-    public var animatedUIImage: UIImage? {
+    public var uiImage: UIImage? {
         switch frameCount {
             case 0:
                 return nil
-
             case 1:
-                return createFrameUIImage(at: 0)
-
+                return staticUIImage
             default:
-                var duration: TimeInterval = 0.0
-                var images: [UIImage] = []
-
-                for i in 0..<frameCount {
-                    guard let image = createFrameUIImage(at: i) else {
-                        continue
-                    }
-
-                    images.append(image)
-                    duration += frameDuration(at: i) ?? 0.0
-                }
-
-                return UIImage.animatedImage(with: images, duration: duration)
+                return animatedUIImage
         }
     }
 
-    public var uiImage: UIImage? {
+    public var animatedUIImage: UIImage? {
+        guard frameCount > 1 else {
+            return nil
+        }
+
+        var duration: TimeInterval = 0.0
+        var images: [UIImage] = []
+
+        for i in 0..<frameCount {
+            guard let image = createFrameUIImage(at: i) else {
+                continue
+            }
+
+            images.append(image)
+            duration += frameDuration(at: i) ?? 0.0
+        }
+
+        return UIImage.animatedImage(with: images, duration: duration)
+    }
+
+    public var staticUIImage: UIImage? {
         frameCount > 0 ? createFrameUIImage(at: 0) : nil
     }
 
